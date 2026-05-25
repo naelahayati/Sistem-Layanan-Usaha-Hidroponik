46049f14e7e77e7708f7e027d040aa66dbf41636
Naela Hayati <hayatnaelah1@gmail.com>
2026-05-25 13:58:05 +0700

    fix trust proxies dan hapus midtrans

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->validateCsrfTokens(except: [
-            '/midtrans/callback',
-        ]);
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'cek_pengguna' => \App\Http\Middleware\EnsureUserIsPengguna::class,
         ]);
