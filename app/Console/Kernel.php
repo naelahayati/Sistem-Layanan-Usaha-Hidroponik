f5330014e52e22731d0f0656ef9870488109d5a3
Naela Hayati <hayatnaelah1@gmail.com>
2026-05-25 12:20:15 +0700

    fix duplicate Kernel class

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
     protected function schedule(Schedule $schedule)
     {
         // Cron QRIS expired
