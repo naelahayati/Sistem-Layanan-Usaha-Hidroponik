f76bdf6ff7bfdf238f1e95c8feb9025b1baada41
Naela Hayati <hayatnaelah1@gmail.com>
2026-06-08 10:01:55 +0700

    fix fontawesome cdn

--- resources/views/admin/Theme/defualt.blade.php
+++ resources/views/admin/Theme/defualt.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
-    <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet"
         href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
