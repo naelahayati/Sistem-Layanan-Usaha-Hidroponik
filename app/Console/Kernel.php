4946ca55d9542695205cbd86eec8d1cfa8e6157c
Naela Hayati <hayatnaelah1@gmail.com>
2026-06-22 11:52:36 +0700

    yuk lah bener

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,10 +14,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('orders:check-expired')->everyMinute();
         $schedule->command('magang:expire-payments')->everyMinute();
 
-        // Laporan Harian Admin ke WhatsApp (Jam 8 Pagi, 1 Siang, 7 Malam)
-        $schedule->command('admin:daily-report')->dailyAt('08:00');
-        $schedule->command('admin:daily-report')->dailyAt('13:00');
-        $schedule->command('admin:daily-report')->dailyAt('19:00');
+        // Laporan Harian Admin ke WhatsApp (Tes Tiap 5 Menit)
+        $schedule->command('admin:daily-report')->everyFiveMinutes();
     }
 
     protected function commands()
