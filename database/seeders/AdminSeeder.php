f842362bfbd0e65b98f8ac3367270304fed64a5b
Naela Hayati <hayatnaelah1@gmail.com>
2026-07-08 11:15:40 +0700

    ipp

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -18,7 +18,7 @@ class AdminSeeder extends Seeder
             ['username' => 'admin'],
             [
                 'name' => 'Admin',
-                'email' => 'admin.@example.com',
+                'email' => 'admin@nazfram.local',
                 'password' => Hash::make('Admin14#'),
                 'role' => 'admin',
                 'status' => 'active',
